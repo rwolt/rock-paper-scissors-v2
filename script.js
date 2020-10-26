d907504e56e5d81a721d9f08f3b99578cc1f1070
Raymond Wolt <rjwolt@gmail.com>
2020-10-26 18:39:19 -0500

    Added animation for single round

--- script.js
+++ script.js
@@ -12,23 +12,30 @@
     let computerChoice = document.getElementById('computer-choice');
 
     rock.addEventListener('click', function(e) {
-        let playerChoice = e.target.id;
-        msg.textContent = roShambo(playerChoice, computerPlay(objects));
-        playerScore.textContent = win;
-        computerScore.textContent = lose;
+        e.target.classList.add('slideinRight');
+        scissors.style.display = 'none';
+        paper.style.display = 'none';
+        msg.textContent = roShambo(e.target.id, computerPlay(objects));
      });
  
      paper.addEventListener('click', function(e) {
+        e.target.classList.add('slideinRight');
+        scissors.style.display = 'none';
+        rock.style.display = 'none';
         msg.textContent = roShambo(e.target.id, computerPlay(objects));
      });
  
      scissors.addEventListener('click', function(e) {
+        e.target.classList.add('slideinRight');
+        rock.style.display = 'none';
+        paper.style.display = 'none';
         msg.textContent = roShambo(e.target.id, computerPlay(objects));
      });
     
 
 
     function roShambo (playerSelection, computerSelection) {
+        
         if (win == 5) {
             return 'You win 5, you are the Winner!'
         }
@@ -36,43 +43,43 @@
             return 'Computer wins 5, You Lose!';
         }
         else if (playerSelection === 'rock' && computerSelection === 'paper') {
-            computerChoice.className = 'paper';
+            computerChoice.classList.add('paper', 'slideinLeft');
             lose ++;
             series ++;
             return 'Computer throws paper. Paper covers rock. You Lose!';       
         }else if (playerSelection === 'rock' && computerSelection === 'scissors') { 
-            computerChoice.className = 'scissors';
+            computerChoice.classList.add('scissors', 'slideinLeft');
             win ++;
             series ++;      
             return 'Computer throws scissors. Rock crushes scissors. You Win!';
         }else if (playerSelection === 'rock' && computerSelection === 'rock') {
-            computerChoice.className = 'rock';
+            computerChoice.classList.add('rock', 'slideinLeft');
             return 'Computer also throws rock. Try again.';
         }else if (playerSelection === 'paper' && computerSelection === 'rock') {
-            computerChoice.className = 'rock';
+            computerChoice.classList.add('rock', 'slideinLeft');
             win++;
             series++;
             return 'Computer throws rock. Paper covers rock. You Win!';
         }else if (playerSelection === 'paper' && computerSelection === 'paper') { 
-            computerChoice.className = 'paper';
+            computerChoice.classList.add('paper', 'slideinLeft');
             return 'Computer also throws paper. Try again.';
         }else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-            computerChoice.className = 'scissors';
+            computerChoice.classList.add('scissors', 'slideinLeft');
             lose++;
             series++;
             return 'Computer throws scissors. Scissors cuts paper. You Lose!';
         }else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-            computerChoice.className = 'rock';
+            computerChoice.classList.add('rock', 'slideinLeft');
             lose++;
             series++;
             return 'Computer throws rock. Rock crushes scissors. You Lose!';
         }else if (playerSelection === 'scissors' && computerSelection === 'paper') { 
-            computerChoice.className = 'paper';
+            computerChoice.classList.add('paper', 'slideinLeft');
             win++;   
             series++;
             return 'Computer throws paper. Scissors cut paper. You Win!';
         }else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
-            computerChoice.className = 'scissors';
+            computerChoice.classList.add('scissors', 'slideinLeft');
             return 'Computer also throws scissors. Try again';
         }else { 
             return'Something has gone wrong. Choose only Rock, Paper, or Scissors.';
