7ac727cd7452b498741b1af73200eb92ba9b9dfe
Raymond Wolt <rjwolt@gmail.com>
2020-10-26 20:19:03 -0500

    Fixed scoring system

--- script.js
+++ script.js
@@ -1,5 +1,51 @@
+    function computerPlay(choices) {
+        let randomIndex = Math.floor(Math.random() * choices.length);
+        return (choices[randomIndex]);  
+    }
 
-    //Set the initial variable to keep track of the score
+    function playRound(playerSelection, computerSelection) {
+
+        if (playerSelection === 'rock' && computerSelection === 'paper') {
+            computerChoice.classList.add('paper', 'slideinLeft');
+            lose ++;
+            return 'You Lose';       
+        }else if (playerSelection === 'rock' && computerSelection === 'scissors') { 
+            computerChoice.classList.add('scissors', 'slideinLeft');
+            win ++;     
+            return 'You Win';
+        }else if (playerSelection === 'rock' && computerSelection === 'rock') {
+            computerChoice.classList.add('rock', 'slideinLeft');
+            return 'Try again';
+        }else if (playerSelection === 'paper' && computerSelection === 'rock') {
+            computerChoice.classList.add('rock', 'slideinLeft');
+            win++;
+            return 'You Win';
+        }else if (playerSelection === 'paper' && computerSelection === 'paper') { 
+            computerChoice.classList.add('paper', 'slideinLeft');
+            return 'Try again';
+        }else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+            computerChoice.classList.add('scissors', 'slideinLeft');
+            lose++;
+            return 'You Lose';
+        }else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+            computerChoice.classList.add('rock', 'slideinLeft');
+            lose++;
+            return 'You Lose';
+        }else if (playerSelection === 'scissors' && computerSelection === 'paper') { 
+            computerChoice.classList.add('paper', 'slideinLeft');
+            win++;   
+            return 'You Win';
+        }else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
+            computerChoice.classList.add('scissors', 'slideinLeft');
+            return 'Try again';
+        }else { 
+            return'Something has gone wrong. Choose only Rock, Paper, or Scissors.';
+        }
+
+    }
+
+
+    
     let win = 0;
     let lose = 0;
     let series = 0;
@@ -8,84 +54,71 @@
     let rock = document.getElementById('rock');
     let paper = document.getElementById('paper');
     let scissors = document.getElementById('scissors');
-    let msg = document.getElementsByClassName('score-box')[0];
+    let container = document.querySelector('.container');
+    let msg = document.getElementById('message');
     let computerChoice = document.getElementById('computer-choice');
+    let playerScore = document.querySelector('#player-score');
+    let computerScore = document.querySelector('#computer-score');
+    
+
 
     rock.addEventListener('click', function(e) {
         e.target.classList.add('slideinRight');
         scissors.style.display = 'none';
         paper.style.display = 'none';
-        msg.textContent = roShambo(e.target.id, computerPlay(objects));
-     });
+        msg.textContent = playRound(e.target.id, computerPlay(objects));
+        playerScore.textContent = win;
+        computerScore.textContent = lose;
+        e.stopPropagation();
+    });
  
      paper.addEventListener('click', function(e) {
         e.target.classList.add('slideinRight');
         scissors.style.display = 'none';
         rock.style.display = 'none';
-        msg.textContent = roShambo(e.target.id, computerPlay(objects));
+        msg.textContent = playRound(e.target.id, computerPlay(objects));
+        playerScore.textContent = win;
+        computerScore.textContent = lose;
+        e.stopPropagation();
      });
  
      scissors.addEventListener('click', function(e) {
         e.target.classList.add('slideinRight');
         rock.style.display = 'none';
         paper.style.display = 'none';
-        msg.textContent = roShambo(e.target.id, computerPlay(objects));
+        msg.textContent = playRound(e.target.id, computerPlay(objects));
+        playerScore.textContent = win;
+        computerScore.textContent = lose;
+        e.stopPropagation();
      });
     
 
+    
 
-    function roShambo (playerSelection, computerSelection) {
-        
-        if (win == 5) {
-            return 'You win 5, you are the Winner!'
-        }
-        else if (lose == 5) {
-            return 'Computer wins 5, You Lose!';
-        }
-        else if (playerSelection === 'rock' && computerSelection === 'paper') {
-            computerChoice.classList.add('paper', 'slideinLeft');
-            lose ++;
-            series ++;
-            return 'Computer throws paper. Paper covers rock. You Lose!';       
-        }else if (playerSelection === 'rock' && computerSelection === 'scissors') { 
-            computerChoice.classList.add('scissors', 'slideinLeft');
-            win ++;
-            series ++;      
-            return 'Computer throws scissors. Rock crushes scissors. You Win!';
-        }else if (playerSelection === 'rock' && computerSelection === 'rock') {
-            computerChoice.classList.add('rock', 'slideinLeft');
-            return 'Computer also throws rock. Try again.';
-        }else if (playerSelection === 'paper' && computerSelection === 'rock') {
-            computerChoice.classList.add('rock', 'slideinLeft');
-            win++;
-            series++;
-            return 'Computer throws rock. Paper covers rock. You Win!';
-        }else if (playerSelection === 'paper' && computerSelection === 'paper') { 
-            computerChoice.classList.add('paper', 'slideinLeft');
-            return 'Computer also throws paper. Try again.';
-        }else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-            computerChoice.classList.add('scissors', 'slideinLeft');
-            lose++;
-            series++;
-            return 'Computer throws scissors. Scissors cuts paper. You Lose!';
-        }else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-            computerChoice.classList.add('rock', 'slideinLeft');
-            lose++;
-            series++;
-            return 'Computer throws rock. Rock crushes scissors. You Lose!';
-        }else if (playerSelection === 'scissors' && computerSelection === 'paper') { 
-            computerChoice.classList.add('paper', 'slideinLeft');
-            win++;   
-            series++;
-            return 'Computer throws paper. Scissors cut paper. You Win!';
-        }else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
-            computerChoice.classList.add('scissors', 'slideinLeft');
-            return 'Computer also throws scissors. Try again';
-        }else { 
-            return'Something has gone wrong. Choose only Rock, Paper, or Scissors.';
-        }
-    }
 
+    window.addEventListener('click', function (e) {
+        if(win == 5) {
+            msg.textContent = 'YOU ARE THE WINNER';
+        }
+        else if(lose == 5){
+            msg.textContent = 'GAME OVER'
+        }
+        else if (computerChoice.classList.contains('slideinLeft')) {
+            rock.style.display = 'block';
+            paper.style.display = 'block';
+            scissors.style.display = 'block';
+            
+            for (let child of container.children) {
+                child.classList.remove('slideinRight');
+            }
+            msg.textContent = 'Choose rock, paper, or scissors';
+            computerChoice.className = '';
+        }
+    });
+    
+
+
+    
 
     //If the player wins 3, announce the score and the winner
     //if (win === 3) {
@@ -95,9 +128,3 @@
     //        console.log(`Computer wins ${lose} out of 5. COMPUTER IS THE WINNER!`);
     //}
  
-    function computerPlay(choices) {
-        let randomIndex = Math.floor(Math.random() * choices.length);
-        return (choices[randomIndex]);  
-    }
-
-    
