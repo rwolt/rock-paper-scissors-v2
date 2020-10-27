1c509221744183ff9abc88d6fb1b5ddc97f3507d
Raymond Wolt <rjwolt@gmail.com>
2020-10-26 20:55:26 -0500

    Fixed game reset after game over screen

--- script.js
+++ script.js
@@ -44,11 +44,25 @@
 
     }
 
+    function reset() {
+        gameOver = false;
+        rock.style.display = 'block';
+        paper.style.display = 'block';
+        scissors.style.display = 'block';
+            
+        for (let child of container.children) {
+            child.classList.remove('slideinRight');
+            }
+            msg.textContent = 'Choose Rock, Paper, or Scissors';
+            computerChoice.className = '';
+    }
+
 
     
     let win = 0;
     let lose = 0;
     let series = 0;
+    let gameOver = false;
 
     let objects = ['rock', 'paper', 'scissors'];
     let rock = document.getElementById('rock');
@@ -60,6 +74,38 @@
     let playerScore = document.querySelector('#player-score');
     let computerScore = document.querySelector('#computer-score');
     
+
+
+    window.addEventListener('click', function(e) {
+        if (gameOver) {
+            e.stopPropagation();
+            win = 0;
+            lose = 0;
+            playerScore.textContent = win;
+            computerScore.textContent = lose;
+            reset();
+        }
+    }, true);
+
+
+    window.addEventListener('click', function (e) {
+
+        if(win == 5) {
+            msg.textContent = 'YOU ARE THE WINNER';
+            e.stopPropagation();
+            gameOver = true;
+        }
+        else if (lose == 5){
+            msg.textContent = 'GAME OVER';
+            e.stopPropagation();
+            gameOver = true;
+        }
+        else if (computerChoice.classList.contains('slideinLeft')) {
+            e.stopPropagation();
+            reset();            
+        }
+    }, true);
+
 
 
     rock.addEventListener('click', function(e) {
@@ -92,39 +138,5 @@
         e.stopPropagation();
      });
     
-
     
 
-
-    window.addEventListener('click', function (e) {
-        if(win == 5) {
-            msg.textContent = 'YOU ARE THE WINNER';
-        }
-        else if(lose == 5){
-            msg.textContent = 'GAME OVER'
-        }
-        else if (computerChoice.classList.contains('slideinLeft')) {
-            rock.style.display = 'block';
-            paper.style.display = 'block';
-            scissors.style.display = 'block';
-            
-            for (let child of container.children) {
-                child.classList.remove('slideinRight');
-            }
-            msg.textContent = 'Choose rock, paper, or scissors';
-            computerChoice.className = '';
-        }
-    });
-    
-
-
-    
-
-    //If the player wins 3, announce the score and the winner
-    //if (win === 3) {
-    //        console.log(`You won ${win} out of 5. YOU ARE THE WINNER!`)
-    //If the computer wins 3, announce the score and the winner
-    //}else {
-    //        console.log(`Computer wins ${lose} out of 5. COMPUTER IS THE WINNER!`);
-    //}
- 
